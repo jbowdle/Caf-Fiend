27a2cf1ca1923f5d94c2122c1dda348fe5713430
jbowdle <johnwbowdle@gmail.com>
2023-05-17 20:31:45 -0500

    Updated addDrink.js functionality

--- public/js/addDrink.js
+++ public/js/addDrink.js
@@ -1,7 +1,23 @@
 const addDrinkHandler = async(event) => {
   event.preventDefault();
 
-  
+  const beverage = document.querySelector("#beverageName").value.trim();
+  const type = document.querySelector("#beverageType").value.trim();
+
+  if (beverage && type) {
+    const response = await fetch('/api/drinks', {
+      method: 'POST',
+      body: JSON.stringify({ beverage, type }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/create`);
+      alert('Drink created');
+    }
+  }
 };
 
-document.querySelector()
+document
+  .querySelector(".drink-form")
+  .addEventListener("submit", addDrinkHandler);
