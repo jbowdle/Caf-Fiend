398a60df2dec69054971db30cca95083cfe3eddb
jbowdle <johnwbowdle@gmail.com>
2023-05-18 12:49:30 -0500

    created modal for adding drinks

--- public/js/addDrink.js
+++ public/js/addDrink.js
@@ -1,3 +1,17 @@
+const modal = document.querySelector("#addDrinkModal");
+const openModal = document.querySelector("#addDrinkBtn");
+const closeModal = document.querySelector(".close");
+
+openModal.addEventListener("click", () => modal.style.display = "block");
+closeModal.addEventListener("click", () => modal.style.display = "none");
+
+// if modal is open and the user clicks the window outside of it, close the modal
+window.addEventListener("click", e => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
 const addDrinkHandler = async(event) => {
   event.preventDefault();
 
@@ -12,8 +26,7 @@ const addDrinkHandler = async(event) => {
     });
 
     if (response.ok) {
-      document.location.replace(`/create`);
-      alert('Drink created');
+      document.location.reload();
     }
   }
 };
