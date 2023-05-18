a9c40130b1f8a10aad2be50e84053b475ebe2561
WolfSpiderman <wolfspiderman@yahoo.com>
2023-05-17 22:21:34 -0500

    added covert delete route for drinkRoute

--- controllers/api/drinkRoutes.js
+++ controllers/api/drinkRoutes.js
@@ -35,6 +35,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+// finds the average of all currently existing ratings for a drink and updates the current_rating column
 router.put('/:id', async (req, res) => {
   try {
     const drinkId = req.params.id;
@@ -43,7 +44,6 @@ router.put('/:id', async (req, res) => {
       include: [{ model: Rating }],
     });
 
-    
     const currentRatings = drinkToUpdate.ratings.map(rating => {
       return rating.rating;
     });
@@ -58,7 +58,22 @@ router.put('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    const deletedDrink = await Drink.destroy({
+      where: { id: req.params.id }
+    });
+    if (deletedDrink === 0) {
+      res.status(404).json({ message: 'No drink found with that id.'});
+      return;
+    }
+    res.status(200).json({ message: 'Drink successfully destroyed, you monster!'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
