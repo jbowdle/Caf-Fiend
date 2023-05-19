28d586791e48f01656f54d71c65a2d4cf79764bd
WolfSpiderman <wolfspiderman@yahoo.com>
2023-05-18 21:16:47 -0500

    got dashboard drinks and ratings rendering properly

--- controllers/api/ratingRoutes.js
+++ controllers/api/ratingRoutes.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
       rating: req.body.rating,
       review: req.body.review,
       drink_id: req.body.drink_id,
-      user_id: req.session.user_id,
+      user_id: req.body.user_id,
     });
     res.status(200).json(newRating);
   } catch (err) {
