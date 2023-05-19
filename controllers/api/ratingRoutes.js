0e6fe406583412d7a8c88aa3a087908b6e1a72ba
WolfSpiderman <wolfspiderman@yahoo.com>
2023-05-18 22:10:26 -0500

    added put and delete route

--- controllers/api/ratingRoutes.js
+++ controllers/api/ratingRoutes.js
@@ -17,4 +17,34 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    const rating = await Rating.findOne({ where: { id: req.params.id } });
+    if (!rating) {
+      return res.status(404).json({ message: 'Rating not found' });
+    }
+    rating.rating = req.body.rating;
+    rating.review = req.body.review;
+    await rating.save();
+    res.status(200).json(rating);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const rating = await Rating.findOne({ where: { id: req.params.id } });
+    if (!rating) {
+      return res.status(404).json({ message: 'Rating not found' });
+    }
+    await rating.destroy();
+    res.status(200).json({ message: 'Rating deleted successfully' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
